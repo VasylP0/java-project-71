f70045242e8f0fd7199c0897324178f812ad42cb
Vasyl L Polyak <frvpolyak@gmail.com>
2025-04-01 08:20:31 -0400

    Fix all Checkstyle violations and finalize test coverage for Parser

--- app/src/test/java/ParserTest.java
+++ app/src/test/java/ParserTest.java
@@ -1,7 +1,9 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 import java.io.IOException;
 import java.util.Map;
